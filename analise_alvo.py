69d9f58f1ae9ac202171c2c92d8e19f8b7a5a1e0
vitorshaft <vtrshaft@gmail.com>
2022-01-29 14:42:40 -0300

    Inseridos comentarios para analise do alvo

--- analise_alvo.py
+++ analise_alvo.py
@@ -1,41 +1,37 @@
 from cv2 import *
 import matplotlib.pyplot as plt
 
-# initialize the camera
-# If you have multiple camera connected with
-# current device, assign a value in cam_port
-# variable according to that
+#defina cam_port com: 0 - camera principal do PC; 1, 2, 3, ... - cameras adicionais (USB)
 cam_port = 1
 cam = VideoCapture(cam_port)
 
-# reading the input using the camera
+# tenta ler uma imagem pela camera
 result, image = cam.read()
 
-# If image will detected without any error,
-# show result
+#se der tudo certo, inicializa a camera em modo video
 if result:
     while True:
         _, frame = cam.read()
 
-        # showing result, it take frame name and image
-        # output
+        #Mostra oq ue a camera enxerga
         imshow("objeto_alvo", frame)
 
-        # If keyboard interrupt occurs, destroy image
-        # window
+        #aguarda pressionar 'ESC' (codigo 27 ali)
         key = waitKey(1)
 
         if key == 27:
-
+            #salva frame atual com o nome de "objeto_alvi.png"
             imwrite("objeto_alvo.png", frame)
             break
     destroyWindow("objeto_alvo")
 
-# If captured image is corrupted, moving to else part
+# Se der algo errado, exibe essa mensagem
 else:
-    print("No image detected. Please! try again")
+    print("Nenhuma imagem detectada. Tente novamente!")
 
+#Le imagem que acabou de ser criada
 img = imread("objeto_alvo.png",0)
+#exibe histograma da imagem (valor RGB/quantidade de pixels)
 histg = calcHist([img],[0],None,[256],[0,256])
 
 plt.plot(histg)
--- rastreia_cor.py
+++ rastreia_cor.py
@@ -4,45 +4,51 @@ import numpy as np
 import serial
 drimbot = serial.Serial(port='COM7', baudrate=9600)
 
+#cap = cv2.VideoCapture(0)  #para usar a webcam principal do seu PC
 cap = cv2.VideoCapture(1)
 
 # Configura resolucao da camera
-cap.set(3, 480)
-cap.set(4, 320)
+cap.set(3, 480)     #largura
+cap.set(4, 320)     #altura
 
+#analisa uma amostra da imagem
 _, frame = cap.read()
-rows, cols, _ = frame.shape
-x_medium = int(cols / 2)
-center = int(cols / 2)
-position = 90 # degrees
+linhas, cols, _ = frame.shape   #obtem numero de linhas e colunas da imagem
+x_medio = int(cols / 2)         #calcula x do meio da tela
+centro = int(cols / 2)          #mesma coisa, mas essa variavel nao vai mudar
+posicao = 90 # degrees          #valor inicial do motor do robo
 
 while True:
     _, frame = cap.read()
-    hsv_frame = frame
     #cor cinza-escuro
-    #low_cinza = np.array([0,0,5])
-    #high_red = np.array([0,0,100])
-    low_cinza = np.array([15,15,15])
-    high_cinza = np.array([50,50,50])
-    cinza_mask = cv2.inRange(hsv_frame,low_cinza,high_cinza)
-    contours, _ = cv2.findContours(cinza_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key=lambda x:cv2.contourArea(x), reverse=True)
-    for cnt in contours:
+    low_cinza = np.array([15,15,15])    #menor RGB possivel
+    high_cinza = np.array([50,50,50])   #maior RGB possivel
+    cinza_mask = cv2.inRange(frame,low_cinza,high_cinza)    #cria intervalo de cores (mascara)
+    contornos, _ = cv2.findContours(cinza_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) #procura contornos da cor detectada
+    contornos = sorted(contornos, key=lambda x:cv2.contourArea(x), reverse=True)    #inverte a ordem dos contornos
+    
+    '''Para cada contorno:
+        cria regiao retangular ao redor do objeto,
+        calcula centro do objeto,
+        desenha linha verde no centro do objeto,
+        Se o objeto estiver fora do centro (+/- 30 pixels), incrementar ou decrementar motor'''
+
+    for cnt in contornos:
         (x, y, w, h) = cv2.boundingRect(cnt)
         
-        x_medium = int((x + x + w) / 2)
+        x_medio = int((x + x + w) / 2)
         break
     
-    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
+    cv2.line(frame, (x_medio, 0), (x_medio, 480), (0, 255, 0), 2)
     # Move servo motor
-    if x_medium < center -30:
-        position += 2
-    elif x_medium > center + 30:
-        position -= 2
-    pos = str(position)+' '
+    if x_medio < centro -30:
+        posicao += 2
+    elif x_medio > centro + 30:
+        posicao -= 2
+    pos = str(posicao)+' '
     drimbot.write(bytes(pos, 'utf-8'))
 
-    cv2.line(frame, (x_medium, 0), (x_medium, 480), (0, 255, 0), 2)
+    cv2.line(frame, (x_medio, 0), (x_medio, 480), (0, 255, 0), 2)
     
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
