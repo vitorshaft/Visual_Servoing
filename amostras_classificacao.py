05ac3eca28d519d21b051c744b9e4d08167b40bd
vitorshaft <vtrshaft@gmail.com>
2022-02-16 10:50:00 -0300

    Inserido script de amostragem do objeto com camera

--- amostras_classificacao.py
+++ amostras_classificacao.py
@@ -1,9 +1,19 @@
+'''     ANTES DE RODAR O CODIGO CRIE DUAS PASTAS NO MESMO LOCAL DESTE SCRIPT    
+    - As pastas devem se chamar 'p' e 'n' (em minúsculo e sem as aspas)
+    - A 'p' é para imagens do objeto, em vários angulos e ocupando o maximo da tela possivel
+    - A 'n' é para imagens negativas, pode ser tudo que estiver no ambiente, mas não pode
+    ter NENHUMA parte do objeto positivo.
+'''
+
 import cv2
 import numpy as np
+from time import sleep
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(1)   #mudar para VideoCapture(0) caso use webcam
 
 while True:
+    #previa da camera para apontar corretamente
+    #depois de apontar aperte ESC para iniciar
     _, frame = cap.read()
 
     #Mostra o que a camera enxerga
@@ -13,17 +23,23 @@ while True:
     if key == 27:        
         break
 cv2.destroyWindow("objeto_alvo")
-
-for i in range(200):
+#cria 800 imagens JPG
+for i in range(121,800):
     _, frame = cap.read()
 
-    #Mostra oq ue a camera enxerga
+    #Mostra oque a camera enxerga
     cv2.imshow("objeto_alvo", frame)
-    cv2.imwrite("/p/1.jpg", frame)
-    #aguarda pressionar 'ESC' (codigo 27 ali)
+    #mudar o endereco para "/n/..." para imagens negativas
+    endereco = "/n/"+str(i)+".jpg"
+    print(endereco)
+    if _:
+        cv2.imwrite(endereco, frame)
+    #cv2.destroyWindow("objeto_alvo")
+    sleep(0.5)
+    #se pressionar ESC ele para
     key = cv2.waitKey(1)
 
     if key == 27:        
         break
-cv2.destroyWindow("objeto_alvo")
+
 print("amostragem concluida!")
